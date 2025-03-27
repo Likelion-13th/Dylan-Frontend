4cee14ce42eeff0e6707d7fc255871f78f61a284
dh1180 <mjdh1936@gmail.com>
2025-03-27 20:48:18 +0900

    250327 frontend

---
+++ src/component/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                LIKELION PROCESS
+            </div>
+            <div>
+                멋쟁이 사자처럼은 창업 동아리이므로 실제 상품을 판매하지 않으며 연출된 페이지입니다.
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
