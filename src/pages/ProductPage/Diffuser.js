8b82910f3aeff3d701aac951e96d113a691d2d38
dh1180 <mjdh1936@gmail.com>
2025-05-18 16:40:00 +0900

    feat, style: 새로운 상품 페이지 및 결제 모달 컴포넌트 추가, 페이지네이션 추가
    - 디퓨저 및 향수 상품 페이지에 상품 카드 및 배너 컴포넌트 추가
    - 결제 모달 컴포넌트 구현 및 마일리지 사용 기능 추가
    - 새로운 이미지 파일 및 스타일 시트 추가
    - New, 디퓨저, 향수 페이지에 페이지네이션 기능 추가

--- src/pages/ProductPage/Diffuser.js
+++ src/pages/ProductPage/Diffuser.js
@@ -1,10 +1,120 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Banner from "./Banner";
+import ProductCard from "./ProductCard";
+import "../../styles/ProductPage.css"
+import PayModal from "../../component/PayModal";
 
 const Diffuser = () => {
+    const products = [
+        {
+            id: 1,
+            name: "디퓨저",
+            brand: "브랜드",
+            price: 30000,
+            imagePath: "/img/diffuser_1.png",
+            isNew: false,
+        },
+        {
+            id: 2,
+            name: "디퓨저`",
+            brand: "브랜드",
+            price: 30000,
+            imagePath: "/img/diffuser_2.png",
+            isNew: false,
+        },
+        {
+            id: 3,
+            name: "디퓨저",
+            brand: "브랜드",
+            price: 30000,
+            imagePath: "/img/diffuser_3.png",
+            isNew: false,
+        },
+        {
+            id: 4,
+            name: "디퓨저",
+            brand: "브랜드",
+            price: 30000,
+            imagePath: "/img/diffuser_4.png",
+            isNew: false,
+        },
+        {
+            id: 5,
+            name: "디퓨저",
+            brand: "브랜드",
+            price: 30000,
+            imagePath: "/img/diffuser_5.png",
+            isNew: false,
+        },
+    ]
+
+    const [selectedProduct, setSelectedProduct] = useState(null)
+    const [isModalOpen, setModalOpen] = useState(false)
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 15; // 페이지당 15개 상품 표시
+
+    const totalPages = Math.ceil(products.length / itemsPerPage);
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentProducts = products.slice(startIndex, endIndex);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const handleCardClick = (product) => {
+        setSelectedProduct(product)
+        setModalOpen(true)
+    }
+
+    const handleCloseModal = () => {
+        setSelectedProduct(null)
+        setModalOpen(false)
+    }
+    
     return (
         <div>
-            <h1>야 너두 사장될 수 있어!</h1>
-            <p>멋쟁이 사자처럼 13기</p>
+            <Banner title="Diffuser" imagePath={"/banner_diffuser.jpg"} />
+            <div className="product-container">
+                <div className="product-grid">
+                    {currentProducts.map((product) => (
+                        <ProductCard key={product.id} product={product}
+                        onClick={() => handleCardClick(product)} />
+                    ))}
+                </div>
+            </div>
+            {isModalOpen && (
+                <PayModal product={selectedProduct} onClose={handleCloseModal} />
+            )}
+
+            <div className="paging">
+                {currentPage > 1 && (
+                    <button className="fill"
+                    onClick={() => handlePageChange(currentPage-1)}>
+                    Prev
+                    </button>
+                )}
+                {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+                    (pageNumber) => (
+                    <button className={`fill ${pageNumber === currentPage ? "active" : ""}`}
+                        key={pageNumber}
+                        onClick={() => handlePageChange(pageNumber)}
+                    >
+                        {pageNumber}
+                    </button>
+                    )
+                )}
+                {currentPage < totalPages && (
+                    <button className="fill"
+                    onClick={() => handlePageChange(currentPage+1)}>
+                    Next
+                    </button>
+                )}
+            </div>
+            
+        <br /><br />
         </div>
     )
 }
--- src/pages/ProductPage/New.js
+++ src/pages/ProductPage/New.js
@@ -1,5 +1,4 @@
-    import React from "react";
-    import banner from '../../assets/banner/Banner.png'
+    import React, { useState, useEffect } from "react";
     import ae from '../../assets/items/ae.png'
     import by from '../../assets/items/by.png'
     import cha from '../../assets/items/cha.png'
@@ -11,51 +10,94 @@
     import musin from '../../assets/items/musin.png'
     import ssum from '../../assets/items/ssum.png'
     import '../../styles/New.css'
+    import ProductCard from "./ProductCard";
+    import "../../styles/ProductPage.css"
+    import PayModal from "../../component/PayModal";
+    import Banner from "./Banner";
 
     const New = () => {
-        return (
-            <div>
-                <img className="new-banner" src={banner} alt="Banner" /> 
-                <section>
-                <div className="container">
-                    <div className="row row-cols-2 row-cols-md-5 g-4 mt-5">
-                    {[
-                        { name: "딥디크 오르페옹", brand: "딥디크", price: "250,000원", img: deep },
-                        { name: "크리드 어벤투스", brand: "크리드", price: "430,000원", img: creed },
-                        { name: "몽블랑 익스플로러", brand: "몽블랑", price: "50,800원", img: mong },
-                        { name: "이솝 휠", brand: "이솝", price: "105,400원", img: ae },
-                        { name: "에이딕트 멜로우", brand: "다이소", price: "3,900원", img: daiso },
-                        { name: "러쉬 우디", brand: "썸유", price: "74,900원", img: ssum },
-                        { name: "카멜리아", brand: "샤넬", price: "124,900원", img: cha },
-                        { name: "스탠다드 뷰티", brand: "무신사", price: "54,900원", img: musin },
-                        { name: "바이레도 블랑쉬", brand: "바이레도", price: "113,000원", img: by },
-                        { name: "스파이시 머스크", brand: "디올트", price: "64,600원", img: di },
-                    ].map((item, idx) => (
-                        <div className="col" key={idx}>
-                            <div className="card h-100 border-0 shadow-sm">
-                                <img src={item.img} className="card-img-top" alt={item.name} />
-                                <div className="card-body">
-                                    <h6 className="fw-bold mb-1">{item.name}</h6>
-                                    <p className="text-muted small mb-1">{item.brand}</p>
-                                    <p className="fw-semibold text-dark">{item.price}</p>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                    </div>
-                    </div>
-                </section>
+        const products = [
+                        { id: 1, isNew: true, name: "딥디크 오르페옹", brand: "딥디크", price: "250,000원", imagePath: deep },
+                        { id: 2, isNew: true, name: "크리드 어벤투스", brand: "크리드", price: "430,000원", imagePath: creed },
+                        { id: 3, isNew: true, name: "몽블랑 익스플로러", brand: "몽블랑", price: "50,800원", imagePath: mong },
+                        { id: 4, isNew: true, name: "이솝 휠", brand: "이솝", price: "105,400원", imagePath: ae },
+                        { id: 5, isNew: true, name: "에이딕트 멜로우", brand: "다이소", price: "3,900원", imagePath: daiso },
+                        { id: 6, isNew: true, name: "러쉬 우디", brand: "썸유", price: "74,900원", imagePath: ssum },
+                        { id: 7, isNew: true, name: "카멜리아", brand: "샤넬", price: "124,900원", imagePath: cha },
+                        { id: 8, isNew: true, name: "스탠다드 뷰티", brand: "무신사", price: "54,900원", imagePath: musin },
+                        { id: 9, isNew: true, name: "바이레도 블랑쉬", brand: "바이레도", price: "113,000원", imagePath: by },
+                        { id: 10, isNew: true, name: "스파이시 머스크", brand: "디올트", price: "64,600원", imagePath: di },
+                    ]
 
-                <nav className="d-flex justify-content-center mt-5 mb-5">
-                    <ul className="pagination">
-                        <li className="page-item">1</li>
-                        <li className="page-item">2</li>
-                        <li className="page-item">3</li>
-                        <li className="page-item">Next</li>
-                    </ul>
-                </nav>
-            </div>
-        )
+    const [selectedProduct, setSelectedProduct] = useState(null)
+    const [isModalOpen, setModalOpen] = useState(false)
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 15; // 페이지당 15개 상품 표시
+
+    const totalPages = Math.ceil(products.length / itemsPerPage);
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentProducts = products.slice(startIndex, endIndex);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const handleCardClick = (product) => {
+        setSelectedProduct(product)
+        setModalOpen(true)
     }
 
-    export default New;
+    const handleCloseModal = () => {
+        setSelectedProduct(null)
+        setModalOpen(false)
+    }
+    
+    return (
+        <div>
+            <Banner title="New" imagePath={"/banner_New.png"} />
+            <div className="product-container">
+                <div className="product-grid">
+                    {currentProducts.map((product) => (
+                        <ProductCard key={product.id} product={product}
+                        onClick={() => handleCardClick(product)} />
+                    ))}
+                </div>
+            </div>
+            {isModalOpen && (
+                <PayModal product={selectedProduct} onClose={handleCloseModal} />
+            )}
+
+            <div className="paging">
+                {currentPage > 1 && (
+                    <button className="fill"
+                    onClick={() => handlePageChange(currentPage-1)}>
+                    Prev
+                    </button>
+                )}
+                {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+                    (pageNumber) => (
+                    <button className={`fill ${pageNumber === currentPage ? "active" : ""}`}
+                        key={pageNumber}
+                        onClick={() => handlePageChange(pageNumber)}
+                    >
+                        {pageNumber}
+                    </button>
+                    )
+                )}
+                {currentPage < totalPages && (
+                    <button className="fill"
+                    onClick={() => handlePageChange(currentPage+1)}>
+                    Next
+                    </button>
+                )}
+            </div>
+            
+        <br /><br />
+        </div>
+    )
+}
+
+export default New;
